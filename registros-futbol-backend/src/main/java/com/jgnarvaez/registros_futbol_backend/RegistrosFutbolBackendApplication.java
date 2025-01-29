7a2138bd3b30a0d17d569367a7dd71ccd13e32f5
José Gregorio <108169866+jgnarvaez@users.noreply.github.com>
2025-01-29 18:43:45 -0500

    feat(backend): Proyecto backend inicial

---
+++ registros-futbol-backend/src/main/java/com/jgnarvaez/registros_futbol_backend/RegistrosFutbolBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jgnarvaez.registros_futbol_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrosFutbolBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrosFutbolBackendApplication.class, args);
+	}
+
+}
---
+++ registros-futbol-backend/src/test/java/com/jgnarvaez/registros_futbol_backend/RegistrosFutbolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgnarvaez.registros_futbol_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrosFutbolBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
